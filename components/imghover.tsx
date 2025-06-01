e004bdac8e50eeb9290fc8e22d0f1204db557b7d
paruntapa <geniustechmax@gmail.com>
2025-06-01 23:56:09 +0530

    feat: body added

---
+++ components/imghover.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const imghover = () => {
+  return (
+    <section>
+        <div className='profile-images'>
+           <div className="img"></div>
+           <div className="img"></div>
+           <div className="img"></div>
+           <div className="img"></div>
+           <div className="img"></div>
+           <div className="img"></div>
+           <div className="img"></div>
+           <div className="img"></div>
+           <div className="img"></div>
+        </div>
+
+        <div className='profile-names'>
+            <div className="name default"></div>
+            <div className="name"></div>
+            <div className="name"></div>
+            <div className="name"></div>
+            <div className="name"></div>
+            <div className="name"></div>
+            <div className="name"></div>
+            <div className="name"></div>
+            <div className="name"></div>
+        </div>
+    </section>
+  )
+}
+
+export default imghover
